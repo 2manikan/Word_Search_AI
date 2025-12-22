d007f0135b9d88eead7ba028c276844c0dfc5a48
= <2mmanikan@gmail.com>
2025-12-22 13:37:37 -0800

    program so far can find all possible substrings that align with the given word, from one node in the graph

---
+++ word_search_ai.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 21 17:55:01 2025
+
+@author: manid
+"""
+
+
+
+
+#word search 
+graph = {1:['A',{2: 'right',4: 'down'}], 2:['T',{1: 'left',3: 'right',5: 'down'}],3:['R',{2: 'left',6: 'down'}],4:['T',{1: 'up',5: 'right'}],5:['T',{2: 'up',4: 'left',6: 'right'}],6:['T',{3: 'up',5: 'left'}], } #(id: [letter, neighbor_id_list])
+word = "ATE"
+
+
+
+
+#assume starting at index 0 (will iterate this starting. only start at nonzero nodes/letters)
+start = 1 #id=0
+fringe = [(start, None, None)] #(id, direction, letter of word index)
+score = 0
+
+while(len(fringe)!=0):
+    
+    current_id, current_direction, first_index = fringe.pop()
+    print("-----",current_id, current_direction, first_index)
+    #find the next nodes to expand
+    for node in graph[current_id][1]:
+        if graph[current_id][0] in word:
+            if current_direction == None:
+                orig_index = word.index(graph[current_id][0])
+                
+                #getting node's individual value
+                added_score = 0
+                if graph[node][0] == word[orig_index + 1]:
+                    added_score = 1
+                    first_index = orig_index + 1
+                    
+                
+                if added_score > 0:
+                    fringe.append((node, graph[current_id][1][node], first_index))
+            else:
+                
+                if first_index < len(word):
+                    
+                    if graph[current_id][1][node] == current_direction:
+                        added_score = 0
+                        
+                        
+                        if graph[node][0] == word[first_index + 1]:
+                            added_score = 1
+                        
+                        if added_score > 0:
+                            fringe.append((node, current_direction, first_index + 1))
+            
+        
+        
+    
+    
